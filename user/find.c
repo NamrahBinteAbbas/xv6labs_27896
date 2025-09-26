142fc1a31491d4885c3fad6aa368bde9d91efa4a
NamrahBinteAbbas <n.abbas.27896@khi.iba.edu.pk>
2025-09-26 23:17:19 +0500

    assignment completed

--- user/find.c
+++ user/find.c
@@ -7,6 +7,47 @@
 #include "kernel/fcntl.h"
 #include "kernel/param.h"   // for MAXARG
 
+// primitive regular expression matcher (from grep.c)
+
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
+
+// match: search for re anywhere in text
+int match(char *re, char *text)
+{
+  if(re[0] == '^')
+    return matchhere(re+1, text);
+  do {  // must look even if string is empty
+    if(matchhere(re, text))
+      return 1;
+  } while(*text++ != '\0');
+  return 0;
+}
+
+// matchhere: search for re at beginning of text
+int matchhere(char *re, char *text)
+{
+  if(re[0] == '\0')
+    return 1;
+  if(re[1] == '*')
+    return matchstar(re[0], re+2, text);
+  if(re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+    return matchhere(re+1, text+1);
+  return 0;
+}
+
+// matchstar: search for c*re at beginning of text
+int matchstar(int c, char *re, char *text)
+{
+  do {  // a * matches zero or more instances
+    if(matchhere(re, text))
+      return 1;
+  } while(*text!='\0' && (*text++==c || c=='.'));
+  return 0;
+}
+
 char*
 fmtname(char *path)
 {
@@ -82,13 +123,13 @@ find(char *path, char *target, int do_exec, char **cmd, int cmdargc)
 
   switch(st.type){
   case T_FILE:
-    if(strcmp(fmtname(path), target) == 0){
-      if(do_exec){
+    if(match(target, fmtname(path))){
+    if(do_exec){
         run_exec(path, cmd, cmdargc);
-      } else {
+    } else {
         printf("%s\n", path);
-      }
     }
+}
     break;
 
   case T_DIR:
